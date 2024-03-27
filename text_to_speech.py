21eda88b8feb33321a6c82aabb4f3037aa98da7f
Giacinto Tucciarone <g.tucciarone@fleexi.heatlh>
2024-03-27 20:15:23 +0100

    Implemented openai text to speech

---
+++ text_to_speech.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+from openai import OpenAI
+
+
+class TextToSpeech:
+
+    def __init__(self):
+        load_dotenv()
+        self.client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+    def convert_to_speech(self, text):
+        return self.client.audio.speech.create(
+            model="tts-1",
+            voice="echo",
+            input=text
+        )
--- voice_assistant.py
+++ voice_assistant.py
@@ -3,15 +3,24 @@ import gtts
 import vlc
 
 from functions import handle_function
+from text_to_speech import TextToSpeech
 from utils import fetch_function
 from voice_recognition import VoiceRecognition
 from dialogue_management import DialogueManagement
 
 
-def play_audio_from_text(text):
-    tts = gtts.gTTS(text, lang="it")
-    tts.save("audio.mp3")
-    v = vlc.MediaPlayer("audio.mp3")
+def play_audio_from_text(text, use_open_ai=False):
+    path = "audio.mp3"
+
+    if use_open_ai is False:
+        tts = gtts.gTTS(text, lang="it")
+        tts.save(path)
+    else:
+        open_ai_text_to_speech = TextToSpeech()
+        response = open_ai_text_to_speech.convert_to_speech(text)
+        response.stream_to_file(path)
+
+    v = vlc.MediaPlayer(path)
     v.play()
     time.sleep(1)
     while v.is_playing():
@@ -24,7 +33,7 @@ class VoiceAssistant:
         self.dialogue_management = DialogueManagement()
 
     def run(self):
-        play_audio_from_text("Ciao sono Tuccia Assistant. Come posso aiutarti oggi?")
+        play_audio_from_text("Ciao sono Tuccia Assistant. Come posso aiutarti oggi?", use_open_ai=False)
         functions = fetch_function(name="functions.json")
         while True:
             try:
@@ -42,7 +51,7 @@ class VoiceAssistant:
                     if response_text is not None:
                         self.dialogue_management.add_dialogue('assistant', response_text)
 
-                    play_audio_from_text(response_text)
+                    play_audio_from_text(response_text, use_open_ai=False)
 
             except KeyboardInterrupt:
                 break
