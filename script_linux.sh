4243ffa9bac339248ad7d69062fcd5f814463058
Giacinto Tucciarone <g.tucciarone@fleexi.heatlh>
2024-04-07 15:46:57 +0200

    Remove 'espeak' from installation dependencies in script.

--- script_linux.sh
+++ script_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install libasound-dev libportaudio2 libportaudiocpp0 espeak vlc portaudio19-dev -y
+sudo apt-get install libasound-dev libportaudio2 libportaudiocpp0 vlc portaudio19-dev -y
